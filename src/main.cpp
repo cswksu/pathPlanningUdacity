828cc5799953f9b400bec16ef95ad98ce83c8ba4
cswksu <cswksu@gmail.com>
2019-03-05 12:44:08 -0500

    add rotation transformation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
@@ -251,8 +252,15 @@ int main() {
             yPath[i]=xyTemp[1];
             
           }
+          double thetaRotCW=atan2(yPath[projSteps-1]-yPath[0],xPath[projSteps-1]-xPath[0]);
+          vector<double> xTransPath(projSteps), yTransPath(projSteps);
+          for (int i=0; i < projSteps; ++i) {
+            xTransPath[i]=xPath[i]*cos(thetaRotCW)+yPath[i]*sin(thetaRotCW);
+            yTransPath[i]=-xPath[i]*sin(thetaRotCW)+yPath[i]*cos(thetaRotCW);
+            
+          }
           tk::spline s;
-          s.set_points(xPath,yPath);
+          s.set_points(xTransPath,yTransPath);
           for (int i =0; i < 50 - prevPathSize; ++i) {
             if (speed < min_speed) {
               if ((acc < 5.0) && (acc<sqrt(10.0*(max_speed-speed)))) {
@@ -271,13 +279,16 @@ int main() {
             double oldPos_y=pos_y;
             pos_x=xyTemp[0];
             pos_y=xyTemp[1];
+            double rotPos_x=pos_x*cos(thetaRotCW)+pos_y*sin(thetaRotCW);
+            double rotPos_y=s(rotPos_x);
+            pos_y= -rotPos_x*sin(-thetaRotCW)+rotPos_y*cos(-thetaRotCW);
+            
             double speedCheck = sqrt(std::pow((pos_x-oldPos_x)/0.02,2)+std::pow((pos_y-oldPos_y)/0.02,2));
             if (speedCheck > max_speed) {
               double overageRatio=speedCheck/max_speed;
               pos_x=oldPos_x+(pos_x-oldPos_x)/overageRatio;
               pos_y=oldPos_y+(pos_y-oldPos_y)/overageRatio;
             }
-            pos_y=s(pos_x);
             next_x_vals.push_back(pos_x);
             next_y_vals.push_back(pos_y);
             prev_pos_x = oldPos_x;
