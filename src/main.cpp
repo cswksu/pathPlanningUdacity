4eaeef54315b96da814fcde68266076ec9f261d4
cswksu <cswksu@gmail.com>
2019-03-07 15:27:37 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -410,13 +410,15 @@ int main() {
             double deltaXRot=speed*0.02*cos(transHdg);
             pos_x_trans+=deltaXRot;
             pos_y_trans=s(pos_x_trans);*/
+            double deltaX = tempX-pos_x_trans;
+            double deltaY = tempY-pos_y_trans;
             pos_x_trans=tempX;
             pos_y_trans=tempY;
             double oldPos_x=pos_x;
             double oldPos_y=pos_y;
-            pos_x=pos_x_trans*cos(-thetaRotCW)+pos_y_trans*sin(-thetaRotCW)+xOffset;
+            pos_x+=deltaX*cos(-thetaRotCW)+deltaY*sin(-thetaRotCW);
             std::cout << "car's new x position: " << pos_x << std::endl;
-            pos_y= -pos_x_trans*sin(-thetaRotCW)+pos_y_trans*cos(-thetaRotCW)+yOffset;
+            pos_y= -deltaX*sin(-thetaRotCW)+deltaY*cos(-thetaRotCW)+yOffset;
             std::cout << "car's new y position: " << pos_y << std::endl;
             
             /**pos_s+=speed*0.02;
